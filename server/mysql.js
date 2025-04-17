00343de58a1bfea3f93b4b36f23574e9a92d4b30
Hajurian <julianmoet1@gmail.com>
2025-04-17 15:25:00 -0400

    Finished add airplane

--- server/mysql.js
+++ server/mysql.js
@@ -26,6 +26,46 @@ const port = 5000;
 app.listen(port);
 app.use(cors({ origin: "http://localhost:5173" })); //allow cors to localhost
 
+//PROCEDURES
+app.get("/add_airplane", (req, res) => {
+  const {
+    ip_airlineID,
+    ip_tail_num,
+    ip_seat_capacity,
+    ip_speed,
+    ip_locationID,
+    ip_plane_type,
+    ip_model,
+    ip_maintenanced,
+    ip_neo,
+  } = req.query;
+
+  console.log(ip_maintenanced);
+  const sql = `CALL add_airplane(?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  const values = [
+    ip_airlineID,
+    ip_tail_num,
+    parseInt(ip_seat_capacity),
+    parseInt(ip_speed),
+    ip_locationID,
+    ip_plane_type,
+    ip_maintenanced || null,
+    ip_model || null,
+    ip_neo == "true" ? true : false,
+  ];
+
+  connection.query(sql, values, (err, rows) => {
+    if (err) {
+      console.error("Error adding airplane:", err);
+      res
+        .status(500)
+        .json({ success: false, message: "Failed to add airplane" });
+    } else {
+      res.json({ success: true, message: rows });
+    }
+  });
+});
+// VIEWS
 app.get("/flight_in_air", (req, res) => {
   connection.query(
     "select l.departure as departing_from,l.arrival as arriving_at,count(distinct f.flightID) as num_flights,group_concat(distinct f.flightID order by f.flightID asc) as flight_list,MIN(f.next_time) AS earliest_arrival,MAX(f.next_time) AS latest_arrival,GROUP_CONCAT(DISTINCT a.locationID ORDER BY a.locationID desc) AS airplane_list from flight f join route_path r ON f.routeID = r.routeID and f.progress = r.sequence join leg l ON r.legID = l.legID join airplane a ON f.support_airline = a.airlineID and f.support_tail = a.tail_num where f.airplane_status = 'in_flight' group by l.departure, l.arrival;",
@@ -87,7 +127,6 @@ app.get("/route_summary", (req, res) => {
   );
 });
 
-//alternative airport
 app.get("/alternative_airports", (req, res) => {
   connection.query(
     "select a.city, a.state, a.country, count(*) as num_airports,group_concat(a.airportID order by a.airportID) as airport_code_list,group_concat(a.airport_name order by a.airportID) as airport_name_list from airport a group by a.city, a.state, a.country having num_airports > 1",
@@ -101,6 +140,7 @@ app.get("/alternative_airports", (req, res) => {
   );
 });
 
+//RESET PROCEDURE
 app.get("/reset", (req, res) => {
   connection.query("CALL magic44_reset_database_state()", (err, rows) => {
     if (err) {
--- src/procedures/AddAirplane.tsx
+++ src/procedures/AddAirplane.tsx
@@ -1,33 +1,118 @@
+import { useState } from "react";
 import "../App.css";
+
 const AddAirplane = () => {
+  const [airline, setAirline] = useState("");
+  const [tailNum, setTailNum] = useState("");
+  const [seatCap, setSeatCap] = useState("");
+  const [speed, setSpeed] = useState("");
+  const [locationID, setLocationID] = useState("");
+  const [planeType, setPlaneType] = useState("");
+  const [maintained, setMaintained] = useState("");
+  const [model, setModel] = useState("");
+  const [neo, setNeo] = useState("");
+
+  async function handleClick() {
+    if (
+      //null check
+      !airline ||
+      !tailNum ||
+      !seatCap ||
+      !speed ||
+      !locationID ||
+      !planeType
+    ) {
+      alert("Invalid parameters");
+      return;
+    }
+
+    const res = await fetch(
+      "http://localhost:5000/add_airplane?" +
+        new URLSearchParams({
+          ip_airlineID: airline,
+          ip_tail_num: tailNum,
+          ip_seat_capacity: seatCap,
+          ip_speed: speed,
+          ip_locationID: locationID,
+          ip_plane_type: planeType,
+          ip_model: model,
+          ip_maintenanced: maintained,
+          ip_neo: neo.toLowerCase(),
+        })
+    );
+    const data = await res.json();
+    if (data.message instanceof Array) {
+      alert("add failed");
+    } else {
+      alert("Successfully added plane");
+    }
+    console.log(data);
+  }
+
   return (
     <div className="wrapper">
       <div className="inputs">
         <label>Airline ID</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={airline}
+          onChange={(e) => setAirline(e.target.value)}
+        />
         <label>Tail Num</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={tailNum}
+          onChange={(e) => setTailNum(e.target.value)}
+        />
         <label>Seat Cap</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={seatCap}
+          onChange={(e) => setSeatCap(e.target.value)}
+        />
       </div>
       <div className="inputs">
         <label>Speed</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={speed}
+          onChange={(e) => setSpeed(e.target.value)}
+        />
         <label>LocationID</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={locationID}
+          onChange={(e) => setLocationID(e.target.value)}
+        />
         <label>Plane Type</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={planeType}
+          onChange={(e) => setPlaneType(e.target.value)}
+        />
       </div>
       <div className="inputs">
         <label>Maintained</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={maintained}
+          onChange={(e) => setMaintained(e.target.value)}
+        />
         <label>Model</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+        />
         <label>Neo</label>
-        <input type="checkbox" />
+        <input
+          type="text"
+          value={neo}
+          onChange={(e) => setNeo(e.target.value)}
+        />
       </div>
       <div className="inputs">
-        <button>Add</button>
+        <button onClick={handleClick}>Add</button>
       </div>
     </div>
   );
